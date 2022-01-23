fbf86ad39cbca7678927f204fc7678e30879bd3d
AaryanJ45 <59236184+AaryanJ45@users.noreply.github.com>
2022-01-22 22:36:37 -0500

    Update HTML

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div>
+    <body>
+
+      <div className="navbar">
+        <img src = "https://texttrumpet.com/images/trumpet.png" className="topimage"/>
+      </div>   
+
+        <h1>HELLO<br/>Hello HELLO<br/>Hello HELLO<br/>Hello HELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>Hello</h1>
+ 
+    </body>
+  </div>;
+};
 
 export default App;
