e99f4f4274190cdc630696eb1bc84738b31f00a9
AaryanJ45 <59236184+AaryanJ45@users.noreply.github.com>
2022-01-23 00:43:25 -0500

    Creating the navigation bar

---
+++ landingg/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+import React from 'react';
+
+const App = () => {
+  return <div>
+<body>
+
+  <div className="navbar">
+    <img src = "https://texttrumpet.com/images/trumpet.png" className="topimage"/>
+    <p>Original Trumpets</p>
+
+    <nav className="my-navbar">
+      <ul className='no-bullets'>
+        <li>Features &nbsp; </li>
+        <li>How It Works &nbsp;</li>
+        <li>Pricing &nbsp; </li>
+      </ul>
+    </nav>
+  </div>   
+
+  <h1>HELLO<br/>Hello HELLO<br/>Hello HELLO<br/>Hello HELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>HelloHELLO<br/>Hello<br/>Hello<br/>Hello<br/>Hello<br/>Hello<br/>Hello<br/>Hello<br/>Hello<br/>Hello</h1>
+
+</body>
+  </div>;
+};
+
+export default App;
+
